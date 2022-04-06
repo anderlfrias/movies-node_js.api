10280c1f291e3c52d39cb8835b20dac29d8cb164
anderlfrias <anderl.friaas@gmail.com>
2022-04-06 16:57:15 -0400

    Registro de usuario funcionando

--- index.js
+++ index.js
@@ -3,20 +3,21 @@ const app = express();
 const port = 8800;
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const authRoute = require("./src/routes/auth");
 
 dotenv.config();
 
-main().catch((err) => console.log(err));
-
-async function main() {
-    await mongoose
+mongoose
     .connect(process.env.MONGO_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     })
     .then(() => console.log("Connected to MongoDB"))
     .catch((err) => console.log(err));
-}
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
